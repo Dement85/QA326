560fb95ed786606de96e13aa783bd222e009d630
Dement85 <138113097+Dement85@users.noreply.github.com>
2023-11-28 19:26:14 +0900

    Add files via upload

---
+++ ДЗ пайтон/HW_10.py
@@ -0,0 +1,9 @@
+# ----------------задача---------------------#
+# Найдите количество различных элементов данного массива.
+
+
+numbers = [2,3,2,2,6,8,9,10]
+
+unique_numbers = set(numbers)
+print(unique_numbers)
+print(len(unique_numbers))
